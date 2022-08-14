ba21bc1b0f292fe5bc492b76ec0daafaf654b987
alamin <alamin.education1997@gmail.com>
2022-08-14 23:58:20 +0600

    hero-text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,21 @@ const Hero = () => {
     return (
         <div className='hero'>
             <div className="left-h">
-                <Header/>
+                <Header />
+                <div className="the-best-add">
+                    <div></div>
+                    <span>THE BEST FITNESS CLUB IN THE TOWN</span>
+                </div>
+                {/* hero Heading */}
+                <div className="hero-text">
+                    <div>
+                        <span className='stroke-text'>Shape </span>
+                        <span>your</span>
+                    </div>
+                    <div>
+                        <span>Ideal body</span>
+                    </div>
+                </div>
             </div>
             <div className="right-h">
                 <h1>right side hero</h1>

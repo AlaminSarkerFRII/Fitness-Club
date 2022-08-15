8ab07ca87ed3f1a56331f0a593321b17629fe740
alamin <alamin.education1997@gmail.com>
2022-08-15 21:31:04 +0600

    update buttons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,6 +20,30 @@ const Hero = () => {
                     <div>
                         <span>Ideal body</span>
                     </div>
+                    <div>
+                        <span> In here we will help you to shape and build your ideal body and live up your life to fullest</span>
+                    </div>
+                </div>
+                {/* figures */}
+                <div className="figures">
+                    <div>
+                        <span>+140</span>
+                        <span>expert coaches</span>
+                    </div>
+                    <div>
+                        <span>+940</span>
+                        <span>Members joined</span>
+                    </div>
+                    <div>
+                        <span>+100</span>
+                        <span>fitness programs</span>
+                    </div>
+
+                </div>
+                {/* hero buttons */}
+                <div className="hero-buttons">
+                    <button className="btn">Get Started</button>
+                    <button className="btn">See More </button>
                 </div>
             </div>
             <div className="right-h">

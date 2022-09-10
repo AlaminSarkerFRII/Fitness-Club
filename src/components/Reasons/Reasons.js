d5e4e14676909cae53b675475b2716ce09f2f14e
alamin <alamin.education1997@gmail.com>
2022-09-11 00:27:06 +0600

    choosen

--- src/components/Reasons/Reasons.js
+++ src/components/Reasons/Reasons.js
@@ -1,22 +1,54 @@
-import React from 'react'
-import "./Reasons.css"
-import image1 from "../../assets/image1.png"
-import image2 from "../../assets/image2.png"
-import image3 from "../../assets/image3.png"
-import image4 from "../../assets/image4.png"
+import React from "react";
+import "./Reasons.css";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+import image4 from "../../assets/image4.png";
+import tick from "../../assets/tick.png";
+import nb from "../../assets/nb.png";
+import nike from "../../assets/nike.png";
+import adidas from "../../assets/adidas.png";
 
 export const Reasons = () => {
   return (
-    <div className='Reasons' id="reasons">
-        <div className='left-r'>
-            <img src={image1} alt=""/>
-            <img src={image2} alt=""/>
-            <img src={image3} alt=""/>
-            <img src={image4} alt=""/>
+    <div className="Reasons" id="reasons">
+      <div className="left-r">
+        <img src={image1} alt="" />
+        <img src={image2} alt="" />
+        <img src={image3} alt="" />
+        <img src={image4} alt="" />
+      </div>
+      <div className="right-r">
+        <span>SOME REASONS</span>
+        <div>
+          <span className="stroke-text">Why</span>
+          <span>Choose us ?</span>
+          <div className="details-r">
+            <div>
+              <img src={tick} alt="" />
+              <span>OVER 140+ EXPERT COACHS</span>
+            </div>
+            <div>
+              <img src={tick} alt="" />
+              <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+            </div>
+            <div>
+              <img src={tick} alt="" />
+              <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+            </div>
+            <div>
+              <img src={tick} alt="" />
+              <span>RELIABLE PARTNERS</span>
+            </div>
+          </div>
+          <span className="partner">Our Partners</span>
+          <div className="partner-imgs">
+          <img src={nb} alt="" />
+          <img src={adidas} alt="" />
+          <img src={nike} alt="" />
+          </div>
         </div>
-        <div className='right-r'>
-            
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
